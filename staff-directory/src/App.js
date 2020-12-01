2e9fece5dd8bfdb3756bccaae12d8538b0f7e4da
ashlinhanson <ashlinhanson@gmail.com>
2020-12-01 16:20:23 -0600

    table

--- staff-directory/src/App.js
+++ staff-directory/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-// import employees from "./employees.json"
+import employees from "./employees"
 import Header from "./components/Header";
 import Search from "./components/Search";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div>
       <Header/>
       <Search/>
+      <Table employees={employees}/>
     </div>
 
   );
